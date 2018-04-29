d591a957443ca45e6339839ad9376ecc53f98953
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-04-29 22:03:05 +0900

    add tests

--- test/hello_test.exs
+++ test/hello_test.exs
@@ -1,8 +1,26 @@
 defmodule HelloTest do
   use ExUnit.Case
-  doctest Hello
+  use Plug.Test
 
-  test "greets the world" do
-    assert Hello.hello() == :world
+  @opts Hello.Router.init([])
+
+  test "returns hello world" do
+    conn =
+      conn(:get, "/hello")
+      |> Hello.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Hello world!\n"
+  end
+
+  test "returns hello Alice" do
+    conn =
+      conn(:get, "/hello/Alice")
+      |> Hello.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Hello Alice!\n"
   end
 end
